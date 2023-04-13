4e6ab4d1def38e378026aa3fadf982165cfaf362
Jaime Filho <jaimeadanielfilho@hotmail.com>
2023-04-13 13:44:50 -0300

    chore(trabalhos/t1): make output more readable

--- trabalhos/t1/jaimeAntonioDanielFilho.c
+++ trabalhos/t1/jaimeAntonioDanielFilho.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     const double VALOR_LITORAL = 216.72;
@@ -15,7 +16,7 @@ int main() {
     const int PONTOS_CEREAL = 2;
     const int PONTOS_AGUA = 5;
 
-    int qnt_passagens;
+    int qtd_passagens;
     int codigo_destino;
 
     printf("==================== Bem-vindo(a) a Rodoviaria Tux ====================\n");
@@ -34,9 +35,9 @@ int main() {
 
     printf("\n");
     printf("Quantas passagens voce deseja comprar? ");
-    scanf("%d", &qnt_passagens);
+    scanf("%d", &qtd_passagens);
 
-    if (qnt_passagens > 0) {
+    if (qtd_passagens > 0) {
         printf("Qual o codigo de seu destino? ");
         scanf("%d", &codigo_destino);
 
@@ -61,34 +62,38 @@ int main() {
                     break;
             }
 
-            char incluir_volta, incluir_seguro;
+            char resposta_seguro, resposta_volta;
             double valor_emissao = 0.0, valor_seguro = 0.0, valor_desconto = 0.0;
 
             printf("Voce deseja incluir o seguro de vida (s/n)? ");
-            scanf(" %c", &incluir_seguro);
+            scanf(" %c", &resposta_seguro);
 
             printf("Voce deseja comprar tambem passagens de volta (s/n)? ");
-            scanf(" %c", &incluir_volta);
+            scanf(" %c", &resposta_volta);
 
-            if (incluir_volta == 's' || incluir_volta == 'S') {
-                valor_emissao += qnt_passagens * TAXA_EMISSAO_VOLTA;
-                qnt_passagens *= 2;
+            bool incluir_seguro = resposta_seguro == 's' || resposta_seguro == 'S';
+            bool incluir_volta = resposta_volta == 's' || resposta_volta == 'S';
+
+
+            if (incluir_volta) {
+                valor_emissao += qtd_passagens * TAXA_EMISSAO_VOLTA;
+                qtd_passagens *= 2;
             }
 
-            double valor_compra_base = qnt_passagens * valor_passagem;
+            double valor_compra_base = qtd_passagens * valor_passagem;
 
-            if (incluir_seguro == 's' || incluir_seguro == 'S') {
-                valor_seguro += qnt_passagens * (valor_passagem * PERCENTUAL_SEGURO);
+            if (incluir_seguro) {
+                valor_seguro += qtd_passagens * (valor_passagem * PERCENTUAL_SEGURO);
             }
 
-            if (qnt_passagens == 2) {
+            if (qtd_passagens == 2) {
                 valor_desconto += valor_passagem * (9.0 / 100.0);
-            } else if (qnt_passagens == 3) {
+            } else if (qtd_passagens == 3) {
                 valor_desconto += valor_passagem * (11.0 / 100.0);
                 valor_desconto += valor_passagem * (13.0 / 100.0);
-            } else if (qnt_passagens == 4) {
+            } else if (qtd_passagens == 4) {
                 valor_desconto += 3 * valor_passagem * (15.0 / 100.0);
-            } else if (qnt_passagens >= 5) {
+            } else if (qtd_passagens >= 5) {
                 valor_desconto += valor_compra_base * (16.0 / 100.0);
             }
 
@@ -104,7 +109,12 @@ int main() {
             printf("\n");
             printf("######################### Relatorio de Compra #########################\n");
 
-            printf("Valor base da compra:               R$%.2lf\n", valor_compra_base);
+            if (incluir_volta) {
+                printf("Valor base da compra:               R$%.2lf (%d passagens * 2)\n", valor_compra_base, qtd_passagens / 2);
+            } else {
+                printf("Valor base da compra:               R$%.2lf (%d passagem(ns))\n", valor_compra_base, qtd_passagens);
+            }
+
             printf("Valor da compra:                    R$%.2lf\n", valor_compra);
 
             if (valor_desconto > 0) {
@@ -135,7 +145,7 @@ int main() {
                 printf("-> Voce gastou o mesmo que o valor base.\n");
             }
 
-            int qnt_cereal = 0, qnt_agua = 0;
+            int qtd_cereal = 0, qtd_agua = 0;
             int pontos_restantes = pontos;
 
             if (pontos_restantes >= PONTOS_CEREAL) {
@@ -147,34 +157,34 @@ int main() {
                 int max_cereal = pontos_restantes / PONTOS_CEREAL;
 
                 printf("\nQuantas barras de cereal voce deseja (maximo %d): ", max_cereal);
-                scanf("%d", &qnt_cereal); 
+                scanf("%d", &qtd_cereal);
             }
 
-            if (qnt_cereal >= 0) {
-                pontos_restantes -= qnt_cereal * PONTOS_CEREAL;
+            if (qtd_cereal >= 0) {
+                pontos_restantes -= qtd_cereal * PONTOS_CEREAL;
 
                 if (pontos_restantes >= 0) {
                     if (pontos_restantes >= PONTOS_AGUA) {
                         int max_agua = pontos_restantes / PONTOS_AGUA;
 
                         printf("Quantas garrafas de agua voce deseja (maximo %d): ", max_agua);
-                        scanf("%d", &qnt_agua);
+                        scanf("%d", &qtd_agua);
                     }
 
-                    if (qnt_agua >= 0) {
-                        pontos_restantes -= qnt_agua * PONTOS_AGUA;
+                    if (qtd_agua >= 0) {
+                        pontos_restantes -= qtd_agua * PONTOS_AGUA;
 
                         if (pontos_restantes >= 0) {
-                            if (qnt_cereal > 0 || qnt_agua > 0) {
+                            if (qtd_cereal > 0 || qtd_agua > 0) {
                                 printf("\n");
                                 printf("################################ Bonus ################################\n");
-                                
-                                if (qnt_cereal > 0) {
-                                    printf("a) %d barra(s) de cereal\n", qnt_cereal);
+
+                                if (qtd_cereal > 0) {
+                                    printf("a) %d barra(s) de cereal\n", qtd_cereal);
                                 }
 
-                                if (qnt_agua > 0) {
-                                    printf("b) %d garrafa(s) de agua\n", qnt_agua);
+                                if (qtd_agua > 0) {
+                                    printf("b) %d garrafa(s) de agua\n", qtd_agua);
                                 }
 
                                 if (pontos_restantes > 0) {
@@ -185,13 +195,13 @@ int main() {
                             printf("\n");
                             printf("Obrigado por comprar conosco. Nos, da Rodoviaria Tux, te desejamos uma otima viagem.\n");
                         } else {
-                            printf("ERRO: Voce nao possui pontos suficiente para trocar por %d garrafas de agua.\n", qnt_agua);
+                            printf("ERRO: Voce nao possui pontos suficiente para trocar por %d garrafas de agua.\n", qtd_agua);
                         }
                     } else {
                         printf("ERRO: A quantidade de garrafas de agua precisa maior ou igual a 0.\n");
                     }
                 } else {
-                    printf("ERRO: Voce nao possui pontos suficiente para trocar por %d barras de cereal.\n", qnt_cereal);
+                    printf("ERRO: Voce nao possui pontos suficiente para trocar por %d barras de cereal.\n", qtd_cereal);
                 }
             } else {
                 printf("ERRO: A quantidade de barras de cereais precisa maior ou igual a 0.\n");

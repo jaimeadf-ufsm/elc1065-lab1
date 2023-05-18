ad3ae5eaf410e8909e59279f0529c32a60fd4058
Jaime Filho <jaimeadanielfilho@hotmail.com>
2023-05-18 02:34:34 -0300

    chore(trabalhos/t4): improve occurrences algorithm

--- trabalhos/t4/jaimeAntonioDanielFilho.c
+++ trabalhos/t4/jaimeAntonioDanielFilho.c
@@ -26,47 +26,34 @@ void LerNotas(double *notas, int n)
     printf("\n");
 }
 
-int BuscarNota(double *notas, int n, double nota)
+void AnalisarFrequencias(double *notas, int *frequencias, int n)
 {
     for (int i = 0; i < n; i++)
     {
-        if (notas[i] == nota)
-        {
-            return i;
-        }
+        frequencias[i] = -1;
     }
-
-    return -1;
-}
-
-int AnalisarFrequencias(double *notas, double *repetidas, int *frequencias, int n)
-{
-    int n_repetidas = 0;
 
     for (int i = 0; i < n; i++)
     {
-        double nota = notas[i];
-        int i_repetida = BuscarNota(repetidas, n_repetidas, nota);
-
-        if (i_repetida == -1)
+        if (frequencias[i] == -1)
         {
-            i_repetida = n_repetidas++;
+            frequencias[i] = 1;
 
-            repetidas[i_repetida] = nota;
-            frequencias[i_repetida] = 0;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (notas[i] == notas[j])
+                {
+                    frequencias[i]++;
+                    frequencias[j] = 0;
+                }
+            }
         }
-
-        frequencias[i_repetida]++;
     }
-
-    return n_repetidas;
 }
 
 int main()
 {
     double notas[TAM_NOTAS];
-
-    double repetidas[TAM_NOTAS];
     int frequencias[TAM_NOTAS];
 
     printf("============== Sistema do INEP ==============\n");
@@ -74,23 +61,25 @@ int main()
     int n = LerQuantidadeDeNotas();
 
     LerNotas(notas, n);
+    AnalisarFrequencias(notas, frequencias, n);
 
-    int n_repetidas = AnalisarFrequencias(notas, repetidas, frequencias, n);
+    double nota_mais_frequente = notas[0];
+    int frequencia_maxima = frequencias[0];
 
-    double nota_mais_frequente = repetidas[0];
-    int maior_frequencia = frequencias[0];
-
-    for (int i = 0; i < n_repetidas; i++)
+    for (int i = 0; i < n; i++)
     {
-        double nota = repetidas[i];
+        double nota = notas[i];
         int frequencia = frequencias[i];
 
-        printf("%.1lf ocorre %d vez(es)\n", nota, frequencia);
-
-        if (frequencia > maior_frequencia || frequencia == maior_frequencia && nota > nota_mais_frequente)
+        if (frequencia > 0)
         {
-            nota_mais_frequente = nota;
-            maior_frequencia = frequencia;
+            printf("%.1lf ocorre %d vez(es)\n", nota, frequencia);
+
+            if (frequencia > frequencia_maxima || frequencia == frequencia_maxima && nota > nota_mais_frequente)
+            {
+                nota_mais_frequente = nota;
+                frequencia_maxima = frequencia;
+            }
         }
     }
 
